3ef891e89ff0c13c4aa0d0f434955d7269c35714
Tsaiyou Yuan <pp201266@hotmail.com>
2017-12-11 00:56:18 +0800

    Adjust style code

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -3,6 +3,20 @@ import styled from 'styled-components'
 
 const Input = styled.input`
     padding: 5px 10px;
+    margin: 8px;
+    appearance: none;
+    cursor: pointer;
+    background: #fefefe;
+    color: #333;
+    border: 1px solid #eee;
+    border-radius: 2px;
+    font-size: 13px;
+    transition: all 0.2s;
+
+    &:hover {
+        background: #222;
+        color: #fff;
+    }
 `;
 
 class Button extends React.Component {
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import style from './Header.styl'
+import styled from 'styled-components'
+
+const HeadNav = styled.header`
+    width: 100%;
+    padding: 8px;
+    background: #eee;
+    box-shadow: 0 2px 5px rgba(0,0,0,0.15);
+`;
+
 
 class Header extends React.Component{
     render () {
         return (
-            <header className="header">{this.props.name}</header>
+            <HeadNav className="header" >{this.props.name}</HeadNav>
         );
     }
 }
